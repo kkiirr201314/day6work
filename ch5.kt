2cc28bb2c362b02a4902e4195a05d03b66bc8d66
“kkiirr201314” <qq255073@gmail.com>
2021-06-22 10:54:10 +0800

    c5-5

--- ch5.kt
+++ ch5.kt
@@ -1,7 +1,8 @@
 fun main(){
-    val greetingFunction:(String) -> String = {
+    val greetingFunction:(String,Int) -> String = { playerName,numBulidings ->
         val currentYear = 2021
-        "歡迎光臨 SimVillage ，$it！ (copyrigth $currentYear)"
+        println("新建 $numBulidings 棟房屋")
+        "歡迎光臨 SimVillage ，$playerName！ (copyrigth $currentYear)"
     }
-    println(greetingFunction("李政龍"))
+    println(greetingFunction("李政龍",2))
 }
