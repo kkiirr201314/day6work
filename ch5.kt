21fad85fe1c0052a6b8f617955e296103f4e021e
“kkiirr201314” <qq255073@gmail.com>
2021-06-22 10:45:16 +0800

    c5-2

--- ch5.kt
+++ ch5.kt
@@ -1,6 +1,7 @@
 fun main(){
-    println({
+    val greetingFunction:() -> String = {
         val currentYear = 2021
         "歡迎光臨 SimVillage ，鎮長！ (copyrigth $currentYear)"
-    }())
+    }
+    println(greetingFunction())
 }
