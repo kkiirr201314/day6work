a4247ba1c124bc28690628a05406043110a03bb2
“kkiirr201314” <qq255073@gmail.com>
2021-06-22 12:01:39 +0800

    c5-7

--- ch5.kt
+++ ch5.kt
@@ -4,5 +4,10 @@ fun main(){
         println("新建 $numBulidings 棟房屋")
         "歡迎光臨 SimVillage ，$playerName！ (copyrigth $currentYear)"
     }
-    println(greetingFunction("李政龍",2))
+    runSimulation("李政龍",greetingFunction)
+}
+fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String)
+{
+    val numberBindings = (1..3).shuffled().last()
+    println(greetingFunction(playerName, numberBindings))
 }
