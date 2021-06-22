3cb8611b263d8411cb55a24eaa8e92cd77936fc2
“kkiirr201314” <qq255073@gmail.com>
2021-06-22 13:22:52 +0800

    c5-11(fall)

--- ch5.kt
+++ ch5.kt
@@ -1,16 +1,17 @@
 import javafx.beans.binding.NumberBinding
 
 fun main(){
-    runSimulation("李政龍") { playerName,numBulidings ->
+    runSimulation("李政龍" ) { playerName,numBulidings ->
         val currentYear = 2021
         println("新建 $numBulidings 棟房屋")
         "歡迎光臨 SimVillage ，$playerName！ (copyrigth $currentYear)"
     }
 
 }
-inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String)
+inline fun runSimulation(playerName: String, costPrinter: (Int) -> Unit, greetingFunction: (String, Int) -> String)
 {
     val numberBindings = (1..3).shuffled().last()
+    costPrinter(numberBindings)
     println(greetingFunction(playerName, numberBindings))
 }
 fun printConstructionCost(numberBindings: Int){
